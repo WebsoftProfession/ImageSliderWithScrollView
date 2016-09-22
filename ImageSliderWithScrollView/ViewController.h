e8f5a5f19e5fbe5ed9154a912c8b6564c479b409
WebsoftProfession <dotsquares@deepaksharma2.local>
2016-09-22 15:38:13 +0530

    Initial Commit

---
+++ ImageSliderWithScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageSliderWithScrollView
+//
+//  Created by Dotsquares on 9/22/16.
+//  Copyright © 2016 Dotsquares. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
